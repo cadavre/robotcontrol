be08e8892e7c0a4582e5e777821072e1b53bbb7d
cad.mortess@gmail.com <cad.mortess@gmail.com@c8c0c9f0-3d48-2f1c-ad3d-2497bd8e929d>
2011-07-05 19:53:50 +0000

    BETA

--- robot_control/atmega_slave/atmega_slave.c
+++ robot_control/atmega_slave/atmega_slave.c
@@ -13,13 +13,13 @@
  * Variables for servo controls
  */
 volatile uint16_t servo_pos_raw[4] = {SERVO_DEFAULT,SERVO_DEFAULT,SERVO_DEFAULT,SERVO_DEFAULT};
-volatile uint8_t servo_speed = SERVO_STEPS_PER_DEG;
+volatile uint8_t servo_speed = 0;
 volatile uint8_t current_servo = 0;
 
 /*
  * Variables for SPI
  */
-volatile uint8_t drive_state[6] = {90,90,90,90,0,SERVO_STEPS_PER_DEG};
+volatile uint8_t drive_state[6] = {0,0,0,0,0,0};
 volatile uint8_t btn_state[6] = {BTN_OFF,BTN_OFF,BTN_OFF,BTN_OFF,BTN_OFF,BTN_OFF};
 
 volatile uint8_t j = 0;
@@ -28,9 +28,6 @@ volatile uint8_t btn_get_flag = 0;
 /*
  * Prototypes
  */
-void btn_stepper_handle(uint8_t btn);
-void btn_servo_handle(uint8_t btn);
-void btn_funct_handle(uint8_t btn);
 void toggle_servo_signal(uint8_t servo);
 void servo_move(uint8_t servo, uint8_t dir);
 void servo_to_deg(uint8_t servo, uint8_t deg);
@@ -42,6 +39,18 @@ void servo_to_raw(uint8_t servo, uint16_t raw);
 void servo_ports_init(void) {
 	DDRD |= SERVOS;
 	PORTD &= ~(SERVOS);
+}
+
+/*
+ * Analog-Digital-Converter interface initialization
+ */
+void ADC_init(void) {
+	ADMUX |= (1<<REFS0)|(1<<REFS1);								// 2,56V
+	ADMUX |= (1<<ADLAR);										// ADCH only
+																// ADC0 by default
+	ADCSRA |= (1<<ADIE);										// ADC interrupt flag
+	ADCSRA |= (1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);		// prescaler 128
+	ADCSRA |= (1<<ADSC);										// initial measurement
 }
 
 /*
@@ -72,11 +81,17 @@ void SPI_init(void) {
 	SPCR |= (1<<SPIE)|(1<<SPE);
 }
 
+/*
+ * Start counting on Timer1
+ */
 void Timer1_start(void) {
 	OCR1A = servo_pos_raw[current_servo];
 	TCCR1B |= (1<<CS10);
 }
 
+/*
+ * Stop counting on Timer1
+ */
 void Timer1_stop(void) {
 	TCCR1B &= ~(1<<CS10);
 	TCNT1 = 0;
@@ -104,8 +119,8 @@ ISR(SPI_STC_vect) {
 
 /*
  * Timer2 interrupt handler
- * - switches servo signal on
- * - turns on Timer1
+ * - switch servo signal on
+ * - turn on Timer1
  */
 ISR(TIMER2_COMP_vect) {
 	toggle_servo_signal(current_servo);
@@ -115,12 +130,22 @@ ISR(TIMER2_COMP_vect) {
 
 /*
  * Timer1 interrupt handler
- * - switches servo signal off
- * - turns off Timer1
+ * - switch servo signal off
+ * - turn off Timer1
  */
 ISR(TIMER1_COMPA_vect) {
 	toggle_servo_signal(current_servo);
 	Timer1_stop();
+}
+
+/*
+ * ADC interrupt handler
+ * - take value as raw 8bit reading
+ * - update SPI data of speed
+ */
+ISR(ADC_vect) {
+	// 0 - 255
+	servo_speed = drive_state[5] = ADCH;
 }
 
 /************************************************* MAIN *************************************************/
@@ -130,31 +155,45 @@ int main(void)
 	Timer2_init();
 	Timer1_init();
 	SPI_init();
+	ADC_init();
 	sei();
 	while(1)
 	{
-		if (btn_get_flag==16) {
+		if (btn_get_flag==BTN_GET_ON) {
+
 			if (btn_state[0]==BTN_L) {
-				drive_state[0]--;
+				servo_move(0,0);
 			} else if (btn_state[0]==BTN_R) {
-				drive_state[0]++;
+				servo_move(0,1);
 			}
 			if (btn_state[1]==BTN_L) {
-				drive_state[1]--;
+				servo_move(1,0);
 			} else if (btn_state[1]==BTN_R) {
-				drive_state[1]++;
+				servo_move(1,1);
 			}
 			if (btn_state[2]==BTN_L) {
-				drive_state[2]--;
+				servo_move(2,0);
 			} else if (btn_state[2]==BTN_R) {
-				drive_state[2]++;
+				servo_move(2,1);
 			}
 			if (btn_state[3]==BTN_L) {
-				drive_state[3]--;
+				servo_move(3,0);
 			} else if (btn_state[3]==BTN_R) {
-				drive_state[3]++;
+				servo_move(3,1);
 			}
+
+			// refresh current drives positions
+			drive_state[0] = (servo_pos_raw[0] - SERVO_MIN) / SERVO_STEPS_PER_DEG;
+			drive_state[1] = (servo_pos_raw[1] - SERVO_MIN) / SERVO_STEPS_PER_DEG;
+			drive_state[2] = (servo_pos_raw[2] - SERVO_MIN) / SERVO_STEPS_PER_DEG;
+			drive_state[3] = (servo_pos_raw[3] - SERVO_MIN) / SERVO_STEPS_PER_DEG;
+
+			// measure value for speed
+			ADCSRA |= (1<<ADSC);
+
+			// clear button flag
 			btn_get_flag = 0;
+
 		}
 	}
 }
