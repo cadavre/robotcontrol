98034550c611b762e2a868f7260ed9883dc9b401
cad.mortess@gmail.com <cad.mortess@gmail.com@c8c0c9f0-3d48-2f1c-ad3d-2497bd8e929d>
2011-09-01 18:31:58 +0000

    steppers control init TODO: - speed regulation - positioning - LCD update

--- robot_control/atmega_slave/atmega_slave_conf.h
+++ robot_control/atmega_slave/atmega_slave_conf.h
@@ -5,25 +5,8 @@
  *     Author: cadavre
  */
 
-
 #ifndef ATMEGA_SLAVE_CONF_H_
 #define ATMEGA_SLAVE_CONF_H_
-
-// oznaczenia przycisków
-#define BTN_J1		0
-#define BTN_J2		1
-#define BTN_D3		2
-#define BTN_J4		3
-#define BTN_FUNCT	4
-
-// stany przycisków
-#define BTN_OFF		0x00		// nic nie wciœniête
-#define BTN_ON		0x01		// funkcyjny wciœniêty
-#define BTN_L		0x01		// wciœniêty Lewy
-#define BTN_R		0x02		// wciœniêty Prawy
-
-// flaga reakcji na wciœniêcie
-#define BTN_GET_ON	4
 
 // porty SPI
 #define MISO		(1<<PB4)
@@ -46,5 +29,59 @@
 #define SERVO_DEFAULT			SERVO_NULL
 #define SERVO_MIN_DEG			0
 #define SERVO_MAX_DEG			200
+
+// krokowe: porty
+#define MOTOR_PD				(1<<PD4)|(1<<PD5)|(1<<PD6)|(1<<PD7)
+#define MOTOR_PC				(1<<PC2)|(1<<PC3)|(1<<PC4)|(1<<PC5)
+
+// krokowe: cewki
+#define M0_A1					(1<<PD4)
+#define M0_B1					(1<<PD5)
+#define M0_A2					(1<<PD6)
+#define M0_B2					(1<<PD7)
+
+#define M1_A1					(1<<PC2)
+#define M1_B1					(1<<PC3)
+#define M1_A2					(1<<PC4)
+#define M1_B2					(1<<PC5)
+
+// krokowe: kroki
+#define M0_STEP1				PORTD |= M0_A1|M0_B1; PORTD &= ~(M0_A2|M0_B2)
+#define M0_STEP2				PORTD |= M0_B1; PORTD &= ~(M0_A1|M0_A2|M0_B2)
+#define M0_STEP3				PORTD |= M0_A2|M0_B1; PORTD &= ~(M0_A1|M0_B2)
+#define M0_STEP4				PORTD |= M0_A2; PORTD &= ~(M0_A1|M0_B1|M0_B2)
+#define M0_STEP5				PORTD |= M0_A2|M0_B2; PORTD &= ~(M0_A1|M0_B1)
+#define M0_STEP6				PORTD |= M0_B2; PORTD &= ~(M0_A1|M0_A2|M0_B1)
+#define M0_STEP7				PORTD |= M0_A1|M0_B2; PORTD &= ~(M0_A2|M0_B1)
+#define M0_STEP8				PORTD |= M0_A1; PORTD &= ~(M0_A2|M0_B1|M0_B2)
+
+#define M1_STEP1				PORTC |= M1_A1|M1_B1; PORTC &= ~(M1_A2|M1_B2)
+#define M1_STEP2				PORTC |= M1_B1; PORTC &= ~(M1_A1|M1_A2|M1_B2)
+#define M1_STEP3				PORTC |= M1_A2|M1_B1; PORTC &= ~(M1_A1|M1_B2)
+#define M1_STEP4				PORTC |= M1_A2; PORTC &= ~(M1_A1|M1_B1|M1_B2)
+#define M1_STEP5				PORTC |= M1_A2|M1_B2; PORTC &= ~(M1_A1|M1_B1)
+#define M1_STEP6				PORTC |= M1_B2; PORTC &= ~(M1_A1|M1_A2|M1_B1)
+#define M1_STEP7				PORTC |= M1_A1|M1_B2; PORTC &= ~(M1_A2|M1_B1)
+#define M1_STEP8				PORTC |= M1_A1; PORTC &= ~(M1_A2|M1_B1|M1_B2)
+
+// krokowe: stany prze³¹czeñ kroków
+#define M0_SWITCH_FLAG			100
+#define M1_SWITCH_FLAG			200
+
+// oznaczenia przycisków
+#define BTN_J1		0
+#define BTN_J2		1
+#define BTN_D3		2
+#define BTN_J4		3
+#define BTN_FUNCT	4
+
+// stany przycisków
+#define BTN_OFF		0x00		// nic nie wciœniête
+#define BTN_ON		0x01		// funkcyjny wciœniêty
+#define BTN_L		0x01		// wciœniêty Lewy
+#define BTN_R		0x02		// wciœniêty Prawy
+
+// flaga reakcji na wciœniêcie
+#define BTN_GET_ON	4
 
 #endif /* ATMEGA_SLAVE_CONF_H_ */
