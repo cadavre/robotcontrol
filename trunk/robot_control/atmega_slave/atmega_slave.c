8722980cd8f8bdadb0513e15773018b7369bfd21
cad.mortess@gmail.com <cad.mortess@gmail.com@c8c0c9f0-3d48-2f1c-ad3d-2497bd8e929d>
2011-09-01 18:31:58 +0000

    steppers control init TODO: - speed regulation - positioning - LCD update

--- trunk/robot_control/atmega_slave/atmega_slave.c
+++ trunk/robot_control/atmega_slave/atmega_slave.c
@@ -17,6 +17,11 @@ volatile uint8_t servo_speed = 0;
 volatile uint8_t current_servo = 0;
 
 /*
+ * Variables for stepping motors controls
+ */
+volatile uint8_t motor_current_step[2] = {1,1};		// 1-8
+
+/*
  * Variables for SPI
  */
 volatile uint8_t drive_state[6] = {0,0,0,0,0,0};
@@ -24,6 +29,7 @@ volatile uint8_t btn_state[6] = {BTN_OFF,BTN_OFF,BTN_OFF,BTN_OFF,BTN_OFF,BTN_OFF
 
 volatile uint8_t j = 0;
 volatile uint8_t btn_get_flag = 0;
+volatile uint8_t stepper_flag = 0;
 
 /*
  * Prototypes
@@ -32,6 +38,8 @@ void toggle_servo_signal(uint8_t servo);
 void servo_move(uint8_t servo, uint8_t dir);
 void servo_to_deg(uint8_t servo, uint8_t deg);
 void servo_to_raw(uint8_t servo, uint16_t raw);
+void motor_make_step(uint8_t motor, uint8_t dir);
+void motor_move(uint8_t motor, uint8_t dir);
 
 /*
  * Initialize ports for servo signal outputs
@@ -39,6 +47,19 @@ void servo_to_raw(uint8_t servo, uint16_t raw);
 void servo_ports_init(void) {
 	DDRD |= SERVOS;
 	PORTD &= ~(SERVOS);
+}
+
+/*
+ * Initialize ports for stepping motors outputs
+ */
+void motor_ports_init(void) {
+	DDRD |= MOTOR_PD;
+	PORTD &= ~(MOTOR_PD);
+	DDRC |= MOTOR_PC;
+	PORTD &= ~(MOTOR_PC);
+	// set ports to default STEP1
+	M0_STEP1;
+	M1_STEP1;
 }
 
 /*
@@ -51,6 +72,14 @@ void ADC_init(void) {
 	ADCSRA |= (1<<ADIE);										// ADC interrupt flag
 	ADCSRA |= (1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);		// prescaler 128
 	ADCSRA |= (1<<ADSC);										// initial measurement
+}
+
+/*
+ * SPI transfer interface initialization
+ */
+void SPI_init(void) {
+	DDRB |= MISO;
+	SPCR |= (1<<SPIE)|(1<<SPE);
 }
 
 /*
@@ -74,18 +103,11 @@ void Timer1_init(void) {
 }
 
 /*
- * Initialize Timer0 for stepping motors switching
+ * Initialize Timer0 for switching steps on stepping motors
  */
 void Timer0_init(void) {
-	TCCR0 |= (1<<CS00)|(1<<CS02);
-}
-
-/*
- * SPI transfer interface initialization
- */
-void SPI_init(void) {
-	DDRB |= MISO;
-	SPCR |= (1<<SPIE)|(1<<SPE);
+	TCCR0 |= (1<<CS00);							// no prescaler
+	TIMSK |= (1<<TOIE0);						// overflow IRQ
 }
 
 /*
@@ -146,6 +168,33 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 /*
+ * Timer0 interrupt handler
+ */
+ISR(TIMER0_OVF_vect) {
+	if (stepper_flag>servo_speed) {
+		stepper_flag = 0;
+	} else {
+		stepper_flag++;
+	}
+
+	if (stepper_flag == 0) {
+		if (btn_state[0]==BTN_L) {
+			motor_move(0,0);
+		} else if (btn_state[0]==BTN_R) {
+			motor_move(0,1);
+		}
+	}
+	if (stepper_flag == 0) {
+		if (btn_state[2]==BTN_L) {
+			motor_move(1,0);
+		} else if (btn_state[2]==BTN_R) {
+			motor_move(1,1);
+		}
+	}
+
+}
+
+/*
  * ADC interrupt handler
  * - take value as raw 8bit reading
  * - update SPI data of speed
@@ -159,8 +208,10 @@ ISR(ADC_vect) {
 int main(void)
 {
 	servo_ports_init();
+	motor_ports_init();
 	Timer2_init();
 	Timer1_init();
+	Timer0_init();
 	SPI_init();
 	ADC_init();
 	sei();
@@ -168,24 +219,19 @@ int main(void)
 	{
 		if (btn_get_flag==BTN_GET_ON) {
 
-			if (btn_state[0]==BTN_L) {
-				servo_move(0,0);
-			} else if (btn_state[0]==BTN_R) {
-				servo_move(0,1);
-			}
+			/* buttons for servos */
 			if (btn_state[1]==BTN_L) {
+				servo_move(0,0);
 				servo_move(1,0);
 			} else if (btn_state[1]==BTN_R) {
+				servo_move(0,1);
 				servo_move(1,1);
 			}
-			if (btn_state[2]==BTN_L) {
-				servo_move(2,0);
-			} else if (btn_state[2]==BTN_R) {
-				servo_move(2,1);
-			}
 			if (btn_state[3]==BTN_L) {
+				servo_move(2,0);
 				servo_move(3,0);
 			} else if (btn_state[3]==BTN_R) {
+				servo_move(2,1);
 				servo_move(3,1);
 			}
 
@@ -229,9 +275,9 @@ void toggle_servo_signal(uint8_t servo) {
  * Changes software PWM overflow controlling servo position, by move
  */
 void servo_move(uint8_t servo, uint8_t dir) {
-	if(dir==1 && servo_pos_raw[servo]<SERVO_MAX) {
+	if (dir==1 && servo_pos_raw[servo]<SERVO_MAX) {
 		servo_pos_raw[servo] += (servo_speed + SERVO_MIN_SPEED);
-	} else if(dir==0 && servo_pos_raw[servo]>SERVO_MIN) {
+	} else if (dir==0 && servo_pos_raw[servo]>SERVO_MIN) {
 		servo_pos_raw[servo] -= (servo_speed + SERVO_MIN_SPEED);
 	}
 }
@@ -240,7 +286,7 @@ void servo_move(uint8_t servo, uint8_t dir) {
  * Changes software PWM overflow controlling servo position, by target deg
  */
 void servo_to_deg(uint8_t servo, uint8_t deg) {
-	if(deg>=SERVO_MIN_DEG && deg<=SERVO_MAX_DEG) {
+	if (deg>=SERVO_MIN_DEG && deg<=SERVO_MAX_DEG) {
 		servo_pos_raw[servo] = SERVO_MIN + (SERVO_STEPS_PER_DEG*deg);
 	}
 }
@@ -250,4 +296,79 @@ void servo_to_deg(uint8_t servo, uint8_t deg) {
  */
 void servo_to_raw(uint8_t servo, uint16_t raw) {
 	servo_pos_raw[servo] = SERVO_MIN + raw;
+}
+
+/*
+ *	Toggle ports states to switch coil set - make step
+ */
+void motor_make_step(uint8_t motor, uint8_t step) {
+	if (step>8) {
+		step = 1;
+	} else if (step<1) {
+		step = 8;
+	}
+	if (motor==0) {
+		switch(step) {
+			case 1:
+				M0_STEP1;
+				break;
+			case 2:
+				M0_STEP2;
+				break;
+			case 3:
+				M0_STEP3;
+				break;
+			case 4:
+				M0_STEP4;
+				break;
+			case 5:
+				M0_STEP5;
+				break;
+			case 6:
+				M0_STEP6;
+				break;
+			case 7:
+				M0_STEP7;
+				break;
+			case 8:
+				M0_STEP8;
+				break;
+		}
+	} else if (motor==1) {
+		switch(step) {
+			case 1:
+				M1_STEP1;
+				break;
+			case 2:
+				M1_STEP2;
+				break;
+			case 3:
+				M1_STEP3;
+				break;
+			case 4:
+				M1_STEP4;
+				break;
+			case 5:
+				M1_STEP5;
+				break;
+			case 6:
+				M1_STEP6;
+				break;
+			case 7:
+				M1_STEP7;
+				break;
+			case 8:
+				M1_STEP8;
+				break;
+		}
+	}
+	motor_current_step[motor] = step;
+}
+
+/*
+ * Move servo in particular directory by requesting step change
+ */
+void motor_move(uint8_t motor, uint8_t dir) {
+	uint8_t next_step = (dir==1) ? motor_current_step[motor]+1 : motor_current_step[motor]-1;
+	motor_make_step(motor, next_step);
 }
